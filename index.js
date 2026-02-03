467da88da8ef9dbfcc91f740028defc856203c8c
Samrat <ksamrat224@gmail.com>
2026-02-03 21:32:07 +0545

    feat: add user and course routes

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express');
-const {createUserRoutes} = require('./user');
-const {createCourseRoutes} = require('./course');
+const {createUserRoutes} = require('./routes/user');
+const {createCourseRoutes} = require('./routes/course');
 const app = express();
 
 createUserRoutes(app);
 createCourseRoutes(app);
-
-
-
 
 app.listen(3000);
 
